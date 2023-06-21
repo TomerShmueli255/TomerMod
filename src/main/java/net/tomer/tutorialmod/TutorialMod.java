55f31bd7d578a6503bea52c594ff06a84e2b2923
TomerShmueli255 <tomer.shmueli11@gmail.com>
2023-06-21 19:48:03 +0300

    Small change

--- src/main/java/net/tomer/tutorialmod/TutorialMod.java
+++ src/main/java/net/tomer/tutorialmod/TutorialMod.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
 {
+    //Some change
     public static final String MOD_ID = "tutorialmod";
     private static final Logger LOGGER = LogUtils.getLogger();
     public TutorialMod()
